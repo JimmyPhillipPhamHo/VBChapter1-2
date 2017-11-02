7332dd48ce84ad1fc0b50e4c3cf4d89db87c517d
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-02 10:45:55 -0500

    Update Math Tutor Application

---
+++ Math Tutor Application/Math Tutor Application/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnShowAnswer_Click(sender As Object, e As EventArgs) Handles btnShowAnswer.Click
+        lblAnswer.Text = "18 + 64 = 82"
+    End Sub
+End Class
