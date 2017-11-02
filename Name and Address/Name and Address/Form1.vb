0d1d0fdd0a6937de39d3b3631165a9a905ef0e20
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-02 10:33:26 -0500

    Update Name and Address

---
+++ Name and Address/Name and Address/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+        lblName.Visible = True
+        lblStreet.Visible = True
+        lblCityStateZip.Visible = True
+    End Sub
+End Class
