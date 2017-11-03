f0a4bff6abb3dee91b66bb5f2680e128268c62c7
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-03 10:32:18 -0500

    Updat to Joke And Punch Line

---
+++ Joke and Punch line/Joke and Punch line/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSetup = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblJoke = New System.Windows.Forms.Label()
+        Me.lblPunchLine = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSetup
+        '
+        Me.btnSetup.Location = New System.Drawing.Point(33, 185)
+        Me.btnSetup.Name = "btnSetup"
+        Me.btnSetup.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetup.TabIndex = 0
+        Me.btnSetup.Text = "Setup"
+        Me.btnSetup.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(165, 185)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Punch Line"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(40, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(359, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Click buttons to display joke and punch line."
+        '
+        'lblJoke
+        '
+        Me.lblJoke.AutoSize = True
+        Me.lblJoke.Location = New System.Drawing.Point(69, 82)
+        Me.lblJoke.Name = "lblJoke"
+        Me.lblJoke.Size = New System.Drawing.Size(289, 13)
+        Me.lblJoke.TabIndex = 3
+        Me.lblJoke.Text = "How many programmers does it take to change a light bulb?"
+        Me.lblJoke.Visible = False
+        '
+        'lblPunchLine
+        '
+        Me.lblPunchLine.AutoSize = True
+        Me.lblPunchLine.Location = New System.Drawing.Point(140, 122)
+        Me.lblPunchLine.Name = "lblPunchLine"
+        Me.lblPunchLine.Size = New System.Drawing.Size(135, 13)
+        Me.lblPunchLine.TabIndex = 4
+        Me.lblPunchLine.Text = "That's a hardware problem."
+        Me.lblPunchLine.Visible = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(305, 185)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 302)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblPunchLine)
+        Me.Controls.Add(Me.lblJoke)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnSetup)
+        Me.Name = "Form1"
+        Me.Text = "Joke and Punch line"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSetup As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblJoke As System.Windows.Forms.Label
+    Friend WithEvents lblPunchLine As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Joke and Punch line/Joke and Punch line/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub btnSetup_Click(sender As Object, e As EventArgs) Handles btnSetup.Click
+        lblJoke.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lblPunchLine.Visible = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
