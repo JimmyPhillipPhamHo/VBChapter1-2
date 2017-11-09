6091ca9820acf273850cd9cb0e9098c56afb5030
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-09 10:04:13 -0600

    Update NAA

--- Name and Address/Name and Address/Form1.vb
+++ Name and Address/Name and Address/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+    Private Sub btnShowInfo_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
         lblName.Visible = True
         lblStreet.Visible = True
         lblCityStateZip.Visible = True
