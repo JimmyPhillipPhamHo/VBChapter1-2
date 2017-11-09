043af7543908ac187d382c09974e548266529c53
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-09 10:02:35 -0600

    Update MT

--- Math Tutor Application/Math Tutor Application/Form1.vb
+++ Math Tutor Application/Math Tutor Application/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
 
